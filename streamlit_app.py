6533b6f4ee1354a035afd2b6e16a181f565508de
Ismet Gocer <127450193+ismetgocer1@users.noreply.github.com>
2023-10-09 21:21:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ selected_model = st.sidebar.selectbox("Choose a Method", ['1.NaiveBayes', '2.Log
 # Text Area
 #user_input = st.text_area("Please enter a review without any punctual or number:")
 #user_input = st.text_area("Please enter a review without any punctual or number:", height=10)
-col1, col2, col3 = st.columns([2,1,1])
+col1, col2, col3 = st.columns([4,1,1])
 with col1:
     user_input = st.text_area("Please enter a review without any punctual or number:", height=4)
 
